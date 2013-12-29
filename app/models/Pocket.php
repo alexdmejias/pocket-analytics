29f61ca1a45c92082bfa92a30dcb527dca2570cd
alex mejias <alexdmejias@gmail.com>
2013-12-29 16:09:30 -0500

    pocket model

--- app/models/Pocket.php
+++ app/models/Pocket.php
@@ -4,5 +4,15 @@ class Pocket extends Eloquent {
 
 	protected $table = 'pockets';
 
+	public static function update_total($new_total) {
+		$last_total = Pocket::first();
+		if (empty($last_total)) {
+			$last_total = new Pocket;
+		}
 
+		$last_total->total = $new_total;
+
+		$last_total->save();
+
+	}
 }
