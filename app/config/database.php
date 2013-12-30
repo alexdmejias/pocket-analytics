98069ddfde91f05368c9f77efe86301cb7e2ee0b
alex mejias <alexdmejias@gmail.com>
2013-12-30 00:26:04 -0500

    update local config files

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'pocket',
+			'database'  => 'l4-test',
 			'username'  => 'root',
 			'password'  => 'root',
 			'charset'   => 'utf8',
