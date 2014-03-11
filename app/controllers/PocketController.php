bfda4532be7bb1e07fedba8c92c40e3a7b23b8d7
alex mejias <alexdmejias@gmail.com>
2014-03-11 15:05:59 -0400

    heavily modified pocket controller in preparation of backbone; new routes in accordance to new methods

--- app/controllers/PocketController.php
+++ app/controllers/PocketController.php
@@ -1,6 +1,6 @@
 <?php
 
-class PocketController extends BaseController {
+class PocketController extends \BaseController {
 
 	public function _config() {
 		$config = array(
@@ -74,9 +74,9 @@ class PocketController extends BaseController {
 		echo $result;
 	}
 
-	public function getArticles($article_count = 10) {
+	public function getPocketArticles($q = 10) {
 		$config = $this->_config();
-		$retrive_url = 'https://getpocket.com/v3/get?count='. $article_count;
+		$retrive_url = 'https://getpocket.com/v3/get?count='. $q;
 
         $data = array(
             'consumer_key' => $config['consumer_key'],
@@ -109,18 +109,50 @@ class PocketController extends BaseController {
 		return (array)$list->list;
 	}
 
-	public function getTotal() {
-		$list = $this->getArticles(10000);
+	public function getTotalPocketArticles() {
+		$list = $this->getPocketArticles(10000);
 
 		return count($list);
 	}
 
-	public function getCounts($q = 10, $m = null) {
+	public function getHome() {
+		$highest = Pocket::orderBy('total', 'desc')->first();
+		$lowest = Pocket::orderBy('total')->first();
+		$latest = Pocket::orderBy('id', 'desc')->first();
+
+		return View::make('home')->with('highest', $highest)->with('lowest', $lowest)->with('latest', $latest);
+
+	}
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index() {
+		return Pocket::orderBy('created_at','desc')->take(10)->get();
+	}
+
+	public function getMultiple($q) {
 		return Pocket::orderBy('created_at','desc')->take($q)->get();
 	}
 
-	public function getInsert($key = '') {
-		$total = $this->getTotal();
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create() {
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store($key = '') {
+		$total = $this->getTotalPocketArticles();
 
 		$pocket = new Pocket;
 
@@ -128,7 +160,10 @@ class PocketController extends BaseController {
 
 		$last_save = Pocket::orderBy('id', 'desc')->first();
 
-		if ($last_save->total != $total) {
+		$config_key = Config::get('pocket.pocket.redirect_uri');
+
+
+		if (($last_save->total != $total) || ($key == $config_key)) {
 			if (!$pocket->save()) {
 				return 'error, could not save';
 			} else {
@@ -139,12 +174,53 @@ class PocketController extends BaseController {
 		}
 	}
 
-	public function getIndex() {
-		$highest = Pocket::orderBy('total', 'desc')->first();
-		$lowest = Pocket::orderBy('total')->first();
-		$latest = Pocket::orderBy('id', 'desc')->first();
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		$url = Pocket::where('id', $id)->take(1)->get();
 
-		return View::make('home')->with('highest', $highest)->with('lowest', $lowest)->with('latest', $latest);
+		return Response::json(array(
+			'error' => false,
+			'urls' => $url->toArray()),
+			200
+		);
 	}
 
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
 }
--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,11 @@
 <?php
 
-Route::get('/', 'PocketController@getIndex');
-Route::controller('pocket', 'PocketController');
+Route::get('/', 'PocketController@getHome');
+
+Route::any('/pocket/store/{key?}', 'PocketController@store');
+Route::get('/pocket/multiple/{q}', 'PocketController@getMultiple');
+Route::get('/pocket/articles/{q}', 'PocketController@getPocketArticles');
+Route::get('/pocket/total', 'PocketController@getTotalPocketArticles');
+Route::get('/pocket/callback', 'PocketController@getCallback');
+
+Route::resource('pocket', 'PocketController');
