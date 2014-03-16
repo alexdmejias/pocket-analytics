5a4857ec0c4fda7b8a0b31d82401ea5e96033614
alex mejias <alexdmejias@gmail.com>
2014-03-16 13:06:10 -0400

    fixed bower; fixing home view to work with new chart; fixing routes

--- app/routes.php
+++ app/routes.php
@@ -2,10 +2,9 @@
 
 Route::get('/', 'PocketController@getHome');
 
+Route::get('/pocket/{q?}', 'PocketController@index');
 Route::any('/pocket/store/{key?}', 'PocketController@store');
-Route::get('/pocket/multiple/{q}', 'PocketController@getMultiple');
+
 Route::get('/pocket/articles/{q}', 'PocketController@getPocketArticles');
 Route::get('/pocket/total', 'PocketController@getTotalPocketArticles');
 Route::get('/pocket/callback', 'PocketController@getCallback');
-
-Route::resource('pocket', 'PocketController');
--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -7,18 +7,36 @@
 <body>
 <style>
 	body {margin:0;}
+	g {
+		fill: none;
+	}
+	g text {
+		fill : black;
+	}
+	line {
+		stroke: #ddd;
+		fill: none;
+	}
+	.mainLine {
+		stroke: red;
+		fill: none;
+		stroke-width: 2px;
+	}
+
+	svg {
+		background :#eee;
+	}
 </style>
 <ul class="stats">
-	<li class="highest">highest {{$highest->total}} on {{$highest->created_at}}</li>
-	<li class="lowest">lowest {{$lowest->total}} on {{$lowest->created_at}}</li>
+	<li class="highest">highest ever {{$highest->total}} on {{$highest->created_at}}</li>
+	<li class="lowest">lowest ever {{$lowest->total}} on {{$lowest->created_at}}</li>
+	<li class="latest">The last count was {{$latest->total}} and was recorded at {{$latest->created_at}}</li>
 	<li class="total">Currently showing <span></span> data points</li>
-	<li class="latest">The last count was {{$latest->total}} ans was recorded at {{$latest->created_at}}</li>
 	<li class="changeFirstLast">change in the first and last points: <span></span></li>
-	<li class="lastTwo">change in the last two points: <span></span></li>
+	<li class="changeLastTwo">change in the last two points: <span></span></li>
 </ul>
 
-	<script src="<?php echo asset('/bower_components/jquery/jquery.js'); ?>"></script>
-	<script src="<?php echo asset('/bower_components/chartjs/Chart.js'); ?>"></script>
+	<script src="<?php echo asset('/bower_components/d3/d3.js'); ?>"></script>
 	<script src="<?php echo asset('/library/js/app.js'); ?>"></script>
 </body>
 </html>
