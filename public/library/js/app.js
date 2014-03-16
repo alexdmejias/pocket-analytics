eca4eda2afa04fe2169c775a70e6e47b330d15a3
alex mejias <alexdmejias@gmail.com>
2014-03-16 13:02:31 -0400

    d3 chart

--- public/library/js/app.js
+++ public/library/js/app.js
@@ -1,117 +1,153 @@
-'use strict';
-
 var app = {
-	json : '',
-	chartInfo : {},
-	data : {},
-	timer : null,
-	totalData : null,
-	defaultRequestNum : 24,
-	hasCanvas: false,
+	data: null, // global variable to house the data
+	width: null,
+	height: 600, // height of the graph
+	margin: {
+		top: 20,
+		right: 20,
+		bottom: 100,
+		left: 50
+	},
+	body: d3.select('body'),
+	svg: null,
+	line: null,
+	x: null,
+	y: null,
+	amountOfData: 40, // # of data points to initially show
 
-	prepareData : function (data) {
-		app.data.labels = [];
-		app.data.totals = [];
-		for (var i = 0; i < app.json.length; i++) {
-			var total = app.json[i].total;
+	/**
+	 * sets up various variables for the graph
+	 * before the ajax request
+	 */
+	setup: function() {
+		app.width= document.documentElement.clientWidth - app.margin.left - app.margin.right;
+		app.height= app.height - app.margin.top - app.margin.bottom;
 
-			app.data.labels.push('['+i+'] '+ app.json[i].created_at.substr(5,11) + ' hours ['+total+']');
-			app.data.totals.push(total);
-		}
+		app.svg = app.body.append('svg')
+			.attr({
+				width: app.width + app.margin.left + app.margin.right,
+				height: app.height + app.margin.top + app.margin.bottom
+			})
+			.append("g").attr("transform", "translate(" + app.margin.left + "," + app.margin.top + ")");
+
+		app.parseDate = d3.time.format('%Y-%m-%d %X').parse;
+
+		app.x = d3.scale.linear()
+			.range([0, app.width]);
+
+		app.y = d3.scale.linear()
+			.range([app.height, 0]);
+
+		app.line = d3.svg.line()
+			.x(function (d, i) { return app.x(i)})
+			.y(function (d) { return app.y(d.total)});
 	},
 
-    genChartOptions : function (labels, data) {
-    	app.chartInfo = {
-			data: {
-				labels : labels,
-				datasets : [
-					{
-						fillColor : "rgba(220,220,220,0.5)",
-						strokeColor : "rgba(220,220,220,1)",
-						pointColor : "rgba(220,220,220,1)",
-						pointStrokeColor : "#fff",
-						data : data
-					}
-				]
-		    },
-			options : {
-				pointDotRadius : 3,
-				scaleOverride : true,
-				scaleSteps : Math.max.apply(null, data) - Math.min.apply(null, data),
-				scaleStepWidth : 1,
-				scaleStartValue : Math.min.apply(null, data)
-			}
-		}
-    },
-
-    makeCanvas : function () {
-    	$('body').append('<canvas id="chart" height="900" width="700" />');
-    	app.hasCanvas = true;
-    },
-
-    resizeCanvas : function () {
-    	$chart = $('#chart');
-    	$chart.attr('width', ($(window).width() - 50));
-    	app.renderChart('chart', app.chartInfo);
-    },
-
-    renderChart : function (el, data) {
-	    return new Chart(document.getElementById(el).getContext("2d")).Line(data.data, data.options);
-    },
-
-	showChange: function (el, index1, index2){
-		if(!app.totalData) {
-			app.getTotalData()
-		}
-
-		if((0 > index1) || (0 > index2)) {
-			alert('not enough data. index1 = '+index1 + ' index2='+index2);
-		} else {
-			change = app.data.totals[index1] - app.data.totals[index2];
-			if(change > 0) {
-				change = '+'+change;
-			}
-		}
-		$('.stats').children(el).children('span').text(change);
-		return 'datapoints user were '+index2+'-'+index1;
+	/* Draws the main graph line */
+	drawMainLine: function() {
+		app.svg.append('path')
+			.datum(app.data)
+			.attr('d', app.line)
+			.attr('class', 'mainLine')
 	},
 
-	getTotalData : function () {
-		app.totalData = app.data.totals.length;
+	/* Draws both of the axes */
+	drawAxis: function () {
+		app.xAxis = d3.svg.axis()
+			.scale(app.x)
+			.tickFormat( function (d, i) { return String(app.data[i].created_at).substr(4,20)})
+			.tickSize( -app.height)
+			.orient("bottom")
 
-		return app.totalData;
+		app.yAxis = d3.svg.axis()
+			.scale(app.y)
+			.tickSize(-app.width)
+			.orient('left')
+
+
+		app.svg.append('g')
+			.attr('transform', 'translate(0,'+app.height+')')
+			.attr('class', 'axis')
+			.call(app.xAxis)
+			.selectAll('text')
+				.attr('transform', 'rotate(-45)')
+				.attr('dx', '-.8em')
+				.style('text-anchor', 'end')
+				.style('font-size', '12px')
+
+		app.svg.append('g')
+			.call(app.yAxis)
 	},
 
-	requestComplete : function (data) {
-		app.json = data;
-		app.prepareData(app.json.reverse());
-		app.genChartOptions(app.data.labels, app.data.totals);
+	drawCircles: function() {
+		app.svg.append('g')
+			.selectAll('circle')
+			.data(app.data)
+			.enter()
+			.append('circle')
+			.attr({
+				cy: function(d) { return app.y(d.total)},
+				cx: function(d, i) { return app.x(i)},
+				r: 5,
+				fill: 'red'
+			})
+/*			.on('mouseover', function (d, i) {
+				d3.select(this).attr({
+					fill: 'blue'
+				})
+			})*/
+	},
 
-		if (!app.hasCanvas) {
-			app.makeCanvas();
-			app.resizeCanvas();
+	drawOthers: function () {
+		// app.svg.append('g')
+			// .selectAll
+	},
+
+	updateHeaderData: function() {
+		var list = document.getElementsByTagName('ul')[0];
+		getStatItemSpan = function(elem, text) {
+			var span = list.getElementsByClassName(elem)[0].getElementsByTagName('span')[0];
+				text = document.createTextNode(text);
+			span.appendChild(text);
 		}
-		app.renderChart('chart', app.chartInfo);
-		app.getTotalData();
-		$('.stats').children('.total').children('span').text(app.totalData);
-		app.showChange('.changeFirstLast', app.totalData - 1, 0);
-		app.showChange('.lastTwo', app.totalData - 1, app.totalData - 2);
+
+		var dataLength = app.data.length;
+		getStatItemSpan('total', dataLength);
+		getStatItemSpan('changeFirstLast', (app.data[dataLength - 1].total - app.data[0].total) );
+		getStatItemSpan('changeLastTwo', (app.data[dataLength - 1].total - app.data[dataLength - 2].total) );
 	},
 
-	makeRequest: function (num) {
-		return $.ajax({
-			url: '/pocket/multiple/'+num,
-		}).promise().done(app.requestComplete);
+	// Callback for when the ajax has been downlaoded
+	done: function() {
+		app.data.forEach( function(d) {
+			d.created_at = app.parseDate(d.created_at);
+			d.total = +d.total;
+		})
 
+		app.x.domain(d3.extent(app.data, function(d, i) { return i; }));
+		app.y.domain(d3.extent(app.data, function(d, i) { return d.total; }));
+
+		app.drawAxis();
+		app.drawMainLine();
+		app.drawCircles();
+		app.drawOthers();
+
+		app.updateHeaderData();
+
+	},
+
+	init: function() {
+		app.setup();
+
+		d3.json("pocket/"+app.amountOfData, function(error, json) {
+
+			app.data = json.reverse();
+			console.log('done loading data')
+			app.done();
+		});
 	}
-
 };
 
-app.makeRequest(app.defaultRequestNum);
-
-$(window).on('resize', function () {
-	clearTimeout(app.timer);
-	app.timer = setTimeout(function () {
-		app.resizeCanvas();
-	}, 300);
-});
+(function() {
+app.init();
+})();
