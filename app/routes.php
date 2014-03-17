1cc088c60340c9b6f330de8fa19f15a4dcd41919
alex mejias <alexdmejias@gmail.com>
2014-03-16 21:36:56 -0400

    fixed routes

--- app/routes.php
+++ app/routes.php
@@ -2,8 +2,8 @@
 
 Route::get('/', 'PocketController@getHome');
 
-Route::get('/pocket/{q?}', 'PocketController@index');
 Route::any('/pocket/store/{key?}', 'PocketController@store');
+Route::get('/pocket/{q?}', 'PocketController@index');
 
 Route::get('/pocket/articles/{q}', 'PocketController@getPocketArticles');
 Route::get('/pocket/total', 'PocketController@getTotalPocketArticles');
